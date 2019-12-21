a9f6f68d23b49a88257a8dfc6415e07c0da34128
bijinyu <156760906@qq.com>
2019-12-21 20:26:22 +0800

    Site updated: 2019-12-21 20:26:19

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/img/trhx2.png");
-        document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+        document.title = '(╯\' - \')╯︵ ┻━┻你丑你就走！';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/img/trhx2.png");
-        document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+        document.title = 'ヾ(◍\'౪`◍)ﾉﾞ你帅就回来！' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
